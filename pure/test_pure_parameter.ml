932e696edb6c8b5b2bdacd669b835fe6f1c01b20
tptiplea <tudor.tiplea96@gmail.com>
2018-05-08 21:18:24 +0100

    test pure param

--- pure/test_pure_parameter.ml
+++ pure/test_pure_parameter.ml
@@ -1,6 +1,6 @@
 (** [ Test parameter server ]  *)
 
-module PS = Actor_param
+module PS = Actor_pure_param
 
 let schedule workers =
   let tasks = List.map (fun x ->
@@ -9,14 +9,14 @@ let schedule workers =
 
 let push _ vars =
   let updates = List.map (fun (k,v) ->
-    Actor_logger.info "working on %i" v;
+    Printf.fprintf Pervasives.stdout "working on %i\n" v; Pervasives.flush Pervasives.stdout;
     (k,v) ) vars in
   updates
 
 let test_context () =
   PS.register_schedule schedule;
   PS.register_push push;
-  PS.start Sys.argv.(1) Actor_config.manager_addr;
-  Actor_logger.info "do some work at master node"
+  PS.start Sys.argv.(1) Actor_pure_config.manager_addr;
+  Printf.fprintf Pervasives.stdout "do some work at master node\n"; Pervasives.flush Pervasives.stdout
 
 let _ = test_context ()
