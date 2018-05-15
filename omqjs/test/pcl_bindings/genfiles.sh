31da25af760f00fecb70662e96783a6bebf63c8f
tptiplea <tudor.tiplea96@gmail.com>
2018-05-15 22:26:02 +0100

    reorganised omq test dir2

--- omqjs/test/pcl_bindings/genfiles.sh
+++ omqjs/test/pcl_bindings/genfiles.sh
@@ -1,4 +1,4 @@
-cp ../../_build/default/omqjs/test_pcl_*.bc .
+cp ../../../_build/default/omqjs/test_pcl_*.bc .
 js_of_ocaml test_pcl_bindings_server.bc --pretty
 js_of_ocaml test_pcl_bindings_client.bc --pretty
 rm test_pcl_*.bc
