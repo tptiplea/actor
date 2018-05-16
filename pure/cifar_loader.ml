b2924fc01e45c64f2386ed79250eaaefa0291163
tptiplea <tudor.tiplea96@gmail.com>
2018-05-17 00:34:49 +0100

    cifar loader random

--- pure/cifar_loader.ml
+++ pure/cifar_loader.ml
@@ -4,7 +4,11 @@ let local_data_path () = failwith "uninmplemented"
     10000 * 3072 matrix. Each row represents a 32 x 32 image of three colour
     channels, unravelled into a row vector. The labels are also returned. *)
 let load_cifar_train_data : int -> (Owl_base_dense_ndarray_s.arr * Owl_base_dense_ndarray_s.arr * Owl_base_dense_ndarray_s.arr) =
-  fun _ -> failwith "uninmplemented"
+  fun _ ->
+    let bsize = 20 in
+    (Owl_base_dense_ndarray.S.uniform [|bsize; 32; 32; 3|],
+     Owl_base_dense_ndarray.S.uniform [|bsize; 1|],
+     Owl_base_dense_ndarray.S.uniform [|bsize; 10|])
 
 let load_cifar_test_data () = failwith "uninmplemented"
 
